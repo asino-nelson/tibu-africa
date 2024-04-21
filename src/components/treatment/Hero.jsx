a3d70687c5e36e7dd096ce7537eb36d4927b6da8
asino-nelson <asinonelson276@gmail.com>
2024-04-21 22:18:01 +0300

    treatment page styling commit

--- src/components/treatment/Hero.jsx
+++ src/components/treatment/Hero.jsx
@@ -1,20 +1,29 @@
-import { FaArrowDown } from 'react-icons/fa6'
+import { FaArrowDown } from "react-icons/fa6";
 
 const Hero = () => {
   return (
-    <div className='h-screen max-h-full flex flex-col items-center py-20'>
-      <div className='bg-cyan-950 w-full h-[50vh] text-white p-10 font-bold rounded-xl grid grid-cols-2'>
-        <p className='flex items-center justify-center font-bold text-6xl m-4'>Healthcere is only a call and click away!</p>
-        <div className='bg-rose-500 w-full h-[90%] rounded-xl'>
-          image
+    <div className="h-screen relative max-h-full flex flex-col items-center py-20">
+      <div className="bg-cyan-950 w-full lg:h-[75%] text-white font-bold rounded-xl lg:grid grid-cols-2 flex flex-col-reverse">
+        <div className="h-full flex items-center text-sm py-3 pr-6 lg:pr-0 pl-6 text-center lg:text-left w-full ">
+          <p className="justify-center font-bold text-4xl lg:text-5xl 2xl:text-6xl p-10">
+            Healthcere is only a call and click away!
+          </p>
+        </div>
+
+        <div className="bg-rose-500 relative h-[25vh] lg:h-full rounded-xl mx-8 mt-10">
+          <div className="h-full w-full relative rounded-2xl lg:rounded-2xl shadow-2xl">
+            image
+          </div>
         </div>
       </div>
-      <div className='flex flex-col items-center justify-center mt-10'>
-        <p>Find a treatment</p>
-        < FaArrowDown/>
+      <div className="hidden absolute inset-x-0 bottom-0 lg:flex justify-center">
+        <div className="flex flex-col items-center justify-center animate-bounce">
+          <p>Find a treatment</p>
+          <FaArrowDown />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/treatment/Treatments.jsx
+++ src/components/treatment/Treatments.jsx
@@ -2,48 +2,53 @@ import HealthCard from "../HealthCard";
 
 const Treatments = () => {
   return (
-    <div>
-      <div className="flec flex-col items-center justify-center">
-        <h1 className="font-bold text-2xl text-emerald-400 my-5">
-          General Health
-        </h1>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 py-2">
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-        </div>
+    <div className="py-7 lg:py-14">
+      <div className="w-full lg:w-2/3 mx-auto">
+        <p className="font-semibold text-2xl lg:text-4xl text-center">
+          Find A Treatment
+        </p>
+        <p className="pt-6 text-base lg:text-xl text-center">
+          Choose a condition to learn more and get started.
+        </p>
       </div>
 
-      <div className="flec flex-col items-center justify-center">
-        <h1 className="font-bold text-2xl text-emerald-400 my-5">
-          Wellness
-        </h1>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 py-2">
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
+      <div className="py-12 flex flex-col gap-y-6">
+        <div className="flec flex-col items-center justify-center space-y-6">
+          <h1 className="font-bold text-2xl text-emerald-400">
+            General Health
+          </h1>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 py-2">
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+          </div>
+        </div>
+
+        <div className="flec flex-col items-center justify-center space-y-6">
+          <h1 className="font-bold text-2xl text-emerald-400">Wellness</h1>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 py-2">
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+          </div>
+        </div>
+
+        <div className="flec flex-col items-center justify-center space-y-6">
+          <h1 className="font-bold text-2xl text-emerald-400 ">
+            Insurance (Life)
+          </h1>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 py-2">
+            <HealthCard page="/" name="Flu and Common Cold" />
+            <HealthCard page="/" name="Flu and Common Cold" />
+          </div>
         </div>
       </div>
-
-      <div className="flec flex-col items-center justify-center">
-        <h1 className="font-bold text-2xl text-emerald-400 my-5">
-          Insurance (Life)
-        </h1>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 py-2">
-          <HealthCard page="/" name="Flu and Common Cold" />
-          <HealthCard page="/" name="Flu and Common Cold" />
-        </div>
-      </div>
-
-
-
-
     </div>
   );
 };
